0e6d4141ead1f35e2b0c9161dd85e4e66444c683
Sasan Azizian <sasan.azizian@gmail.com>
2020-02-21 17:42:26 -0600

    Adjust

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,28 +6,26 @@ public class Test {
         System.out.println(isPalindrome(707));
         System.out.println(isPalindrome(11212));
     }
+
     public class SpeedConverter {
 
-        public long toMilesPerHours(double KilometersPerHour){
-            if( KilometersPerHour < 0){
+        public long toMilesPerHours(double KilometersPerHour) {
+            if (KilometersPerHour < 0) {
                 return -1;
             }
-            return  Math.round(.621371 * KilometersPerHour);
+            return Math.round(.621371 * KilometersPerHour);
         }
     }
 
-    public static boolean isPalindrome(int number){
+    public static boolean isPalindrome(int number) {
 
-        int reverse =0;
-        int num=Math.abs(number);
-
-        while(num > 0){
-
-            int degit = num% 10;
-            reverse =reverse* 10 + degit;
-            num = num/10;
+        int reverse = 0;
+        int num = Math.abs(number);
+        while (num > 0) {
+            int degit = num % 10;
+            reverse = reverse * 10 + degit;
+            num = num / 10;
         }
         return reverse == Math.abs(number);
-
     }
 }
